f92bd46189eadd46037ca1f142ebe1bc03e96473
Lucas Gonzalez Ferreyra <lucasgf2002@gmail.com>
2023-11-09 22:06:46 -0300

    gfgdgdfg

--- MythicOdessey2/Assets/Scripts/Cards/Traps/WaterBalloon.cs
+++ MythicOdessey2/Assets/Scripts/Cards/Traps/WaterBalloon.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 
 public class WaterBalloon : Trap
 {
+    private GameObject _splashVFX;
     private void Awake()
     {
         transform.position = new Vector3(transform.position.x, 10, transform.position.z);   
