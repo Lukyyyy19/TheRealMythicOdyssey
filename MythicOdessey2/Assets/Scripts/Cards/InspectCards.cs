b50f467fa467c7f62280995ffeabc7011e5a880c
Lucas Gonzalez Ferreyra <lucasgf2002@gmail.com>
2023-11-22 18:50:23 -0300

    menu

--- MythicOdessey2/Assets/Scripts/Cards/InspectCards.cs
+++ MythicOdessey2/Assets/Scripts/Cards/InspectCards.cs
@@ -16,9 +16,7 @@ public class InspectCards : MonoBehaviour
     private Vector3 _midPos;
     [SerializeField] private GameObject _textChild;
     [SerializeField] private GameObject _textBackground;
-    [SerializeField] private GameObject _imageBackground;
-    [SerializeField] private GameObject _inspectImageBackground;
-    
+    [SerializeField]private GameObject _backButton;
     RotateCard _rotateCardScript;
 
     private void Awake()
@@ -41,8 +39,7 @@ public class InspectCards : MonoBehaviour
         _button.SetActive(true);
         _textChild.SetActive(true);
         _textBackground.SetActive(true);
-        _imageBackground.SetActive(false);
-        _inspectImageBackground.SetActive(true);
+        _backButton.SetActive(false);
     }
 
     private void OnEnable()
@@ -56,22 +53,27 @@ public class InspectCards : MonoBehaviour
 
         EventManager.instance.AddAction("OnCardDesinspected", (x) =>
         {
-            _rotateCardScript.enabled = false;
-            _textChild.SetActive(false);
-            _textBackground.SetActive(false);
-            _inspectImageBackground.SetActive(false);
-            _imageBackground.SetActive(true);
-            _thisCol.enabled = true;
-            _childCol.enabled = false;
             if (_cardInspected)
             {
                 transform.DOMove(_startPos, .75f);
                 transform.DOScale(.85f, .75f);
-                _child.DORotate(new Vector3(0, 180, 0), .5f);
+                _child.DORotate(new Vector3(0, 180, 0), .5f).OnComplete(() =>
+                {
+                    EventManager.instance.TriggerEvent("OnAppearOtherCards");
+                });
             }
-
+            
+        });
+        EventManager.instance.AddAction("OnAppearOtherCards", (x) =>
+        {
+            _rotateCardScript.enabled = false;
+            _textChild.SetActive(false);
+            _textBackground.SetActive(false);
+            _thisCol.enabled = true;
+            _childCol.enabled = false;
             _cardInspected = false;
             _child.gameObject.SetActive(true);
+            _backButton.SetActive(true);
         });
     }
 
